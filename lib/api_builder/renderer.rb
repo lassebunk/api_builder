f6797edcf43cca037780eed2253b16fa3f2955f4
Lasse Bunk <lassebunk@gmail.com>
2011-09-11 23:36:38 +0200

    fix instance variable name

--- lib/api_builder/renderer.rb
+++ lib/api_builder/renderer.rb
@@ -8,16 +8,16 @@ module ApiBuilder
     attr_reader :out
 
     def initialize(scope, *args, &block)
-      @out = []
+      @_out = []
       ScopeRenderer.eval(self, scope, &block)
     end
 
     def element(*args, &block)
-      @out << ElementRenderer.render(self, *args, &block)
+      @_out << ElementRenderer.render(self, *args, &block)
     end
 
     def array(*args, &block)
-      @out << ArrayRenderer.render(self, *args, &block)
+      @_out << ArrayRenderer.render(self, *args, &block)
     end
   end
 
@@ -31,30 +31,30 @@ module ApiBuilder
 
     def initialize(scope, *args, &block)
       if block_given?
-        @out = {}
+        @_out = {}
         ScopeRenderer.eval(self, scope, &block)
       else
-        @out = args[0]
+        @_out = args[0]
       end
     end
 
     def id(value)
-      @out.update :id => value
+      @_out.update :id => value
     end
 
     def property(hash)
-      @out.update hash
+      @_out.update hash
     end
 
     def array(name, &block)
-      @out.update name => ArrayRenderer.render(self, &block)
+      @_out.update name => ArrayRenderer.render(self, &block)
     end
 
     def method_missing(name, *args, &block)
       if block_given?
-        @out.update name => ElementRenderer.render(self, &block)
+        @_out.update name => ElementRenderer.render(self, &block)
       else
-        @out.update name => args[0]
+        @_out.update name => args[0]
       end
     end
   end
@@ -82,20 +82,20 @@ module ApiBuilder
     end
 
     def element(*args, &block)
-      raise "root element already defined" unless @out.nil?
-      @out = ElementRenderer.render(self, *args, &block)
+      raise "root element already defined" unless @_out.nil?
+      @_out = ElementRenderer.render(self, *args, &block)
     end
 
     def array(&block)
-      raise "root element already defined" unless @out.nil?
-      @out = ArrayRenderer.render(self, &block)
+      raise "root element already defined" unless @_out.nil?
+      @_out = ArrayRenderer.render(self, &block)
     end
   end
   
   class ScopeRenderer
     def self.eval(scope, variable_scope, &block)
       variable_scope.instance_variables.each do |var|
-        scope.instance_variable_set(var, variable_scope.instance_variable_get(var))
+        scope.instance_variable_set(var, variable_scope.instance_variable_get(var)) unless var == "@_out"
       end
       scope.instance_eval(&block)
     end
